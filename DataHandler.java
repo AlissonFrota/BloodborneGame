c8cad28ca4fc304bef0b3f6d74ac73f1d2d6dfcf
Alisson Frota <alissonfrotat@gmail.com>
2025-05-22 08:57:10 -0300

    datahandler

---
+++ DataHandler.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class DataHandler {
+    
+    ArrayList<Arma> Armas = new ArrayList<Arma>();
+
+    ArrayList<Armadura> Armaduras = new ArrayList<Armadura>();
+
+    ArrayList<Ferramenta> Ferramentas = new ArrayList<Ferramenta>();
+
+    ArrayList<Origin> Origins = new ArrayList<Origin>();
+
+    
+
+
+}
